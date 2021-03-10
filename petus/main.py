67324c85e87c97a2fbbbda674695b6abed756608
Milo Weinberg <iapetus011@gmail.com>
2021-03-10 08:18:08 -0500

    fix multiple chunk dumping

--- petus/main.py
+++ petus/main.py
@@ -62,20 +62,20 @@ def noisy_chunk(noise, chunk_x: int, chunk_z: int) -> numpy.ndarray:
 
 
 def dump_to_obj(file, chunks: dict) -> None:
-    amount = len(chunks)
-    joined_chunks = numpy.zeros((256, 16 * amount, 16 * amount))
-
-    for cxz, chunk in chunks.items():
-        cx, cz = cxz
-
-        cx *= -len(chunks) // 2
-        cz *= -len(chunks) // 2
-
-        # print(cz*16, cz*16+16)
-        # print(cx*16, cx*16+16)
-        print(cz, cx)
-
-        joined_chunks[..., cx*16:cx*16+16, cz*16:cz*16+16,] = chunk
+    # amount = len(chunks)
+    # joined_chunks = numpy.zeros((256, 16 * amount, 16 * amount))
+    #
+    # for cxz, chunk in chunks.items():
+    #     cx, cz = cxz
+    #
+    #     cx *= -len(chunks) // 2
+    #     cz *= -len(chunks) // 2
+    #
+    #     # print(cz*16, cz*16+16)
+    #     # print(cx*16, cx*16+16)
+    #     print(cz, cx)
+    #
+    #     joined_chunks[..., cx*16:cx*16+16, cz*16:cz*16+16,] = chunk
 
     points = {}
     rpoints = {}
@@ -92,46 +92,54 @@ def dump_to_obj(file, chunks: dict) -> None:
             faces[len(faces) - 1] = f
             rfaces[f] = len(faces) - 1
 
-    for y in range(joined_chunks.shape[0]):
-        for z in range(joined_chunks.shape[1]):
-            for x in range(joined_chunks.shape[2]):
-                if joined_chunks[y, z, x] == 0:  # air
-                    continue
+    for cx, cz in chunks.keys():
+        for y in range(256):
+            for z in range(16):
+                for x in range(16):
+                    if chunks[cx, cz][y, z, x] == 0:  # air
+                        continue
 
-                append_point(x, y, z)
-                append_point(x + 1, y, z)
-                append_point(x, y + 1, z)
-                append_point(x, y, z + 1)
-                append_point(x + 1, y + 1, z)
-                append_point(x, y + 1, z + 1)
-                append_point(x + 1, y, z + 1)
-                append_point(x + 1, y + 1, z + 1)
+                    tx = x + cx * 16
+                    tz = z + cz * 16
 
-    for y in range(joined_chunks.shape[0]):
-        for z in range(joined_chunks.shape[1]):
-            for x in range(joined_chunks.shape[2]):
-                block = joined_chunks[y, z, x]
+                    append_point(tx, y, tz)
+                    append_point(tx + 1, y, tz)
+                    append_point(tx, y + 1, tz)
+                    append_point(tx, y, tz + 1)
+                    append_point(tx + 1, y + 1, tz)
+                    append_point(tx, y + 1, tz + 1)
+                    append_point(tx + 1, y, tz + 1)
+                    append_point(tx + 1, y + 1, tz + 1)
 
-                if block == 0:  # air
-                    continue
+    for cx, cz in chunks.keys():
+        for y in range(256):
+            for z in range(16):
+                for x in range(16):
+                    block = chunks[cx, cz][y, z, x]
 
-                block = palette[block]
+                    if block == 0:  # air
+                        continue
 
-                i1 = rpoints.get((x, y, z)) + 1
-                i2 = rpoints.get((x + 1, y, z)) + 1
-                i3 = rpoints.get((x, y + 1, z)) + 1
-                i4 = rpoints.get((x, y, z + 1)) + 1
-                i5 = rpoints.get((x + 1, y + 1, z)) + 1
-                i6 = rpoints.get((x, y + 1, z + 1)) + 1
-                i7 = rpoints.get((x + 1, y, z + 1)) + 1
-                i8 = rpoints.get((x + 1, y + 1, z + 1)) + 1
+                    block = palette[block]
 
-                append_face(f"usemtl {block}\nf {i1} {i2} {i7} {i4}")
-                append_face(f"usemtl {block}\nf {i1} {i2} {i5} {i3}")
-                append_face(f"usemtl {block}\nf {i4} {i7} {i8} {i6}")
-                append_face(f"usemtl {block}\nf {i1} {i4} {i6} {i3}")
-                append_face(f"usemtl {block}\nf {i2} {i5} {i8} {i7}")
-                append_face(f"usemtl {block}\nf {i3} {i5} {i8} {i6}")
+                    tx = x + cx * 16
+                    tz = z + cz * 16
+
+                    i1 = rpoints.get((tx, y, tz)) + 1
+                    i2 = rpoints.get((tx + 1, y, tz)) + 1
+                    i3 = rpoints.get((tx, y + 1, tz)) + 1
+                    i4 = rpoints.get((tx, y, tz + 1)) + 1
+                    i5 = rpoints.get((tx + 1, y + 1, tz)) + 1
+                    i6 = rpoints.get((tx, y + 1, tz + 1)) + 1
+                    i7 = rpoints.get((tx + 1, y, tz + 1)) + 1
+                    i8 = rpoints.get((tx + 1, y + 1, tz + 1)) + 1
+
+                    append_face(f"usemtl {block}\nf {i1} {i2} {i7} {i4}")
+                    append_face(f"usemtl {block}\nf {i1} {i2} {i5} {i3}")
+                    append_face(f"usemtl {block}\nf {i4} {i7} {i8} {i6}")
+                    append_face(f"usemtl {block}\nf {i1} {i4} {i6} {i3}")
+                    append_face(f"usemtl {block}\nf {i2} {i5} {i8} {i7}")
+                    append_face(f"usemtl {block}\nf {i3} {i5} {i8} {i6}")
 
     file.write("\n".join([f"v {p[0]} {p[1]} {p[2]}" for p in points.values()]) + "\n" + "\n".join(faces.values()))
 
@@ -140,8 +148,10 @@ with open("test.obj", "w+") as f:
     noise = SimplexNoise()
     chunks = {}
 
-    for x in range(-1, 1):
-        for z in range(-1, 1):
+    radius = 3
+
+    for x in range(-radius, radius):
+        for z in range(-radius, radius):
             chunks[x, z] = noisy_chunk(noise, x, z)
 
     dump_to_obj(f, chunks)
