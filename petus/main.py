c292b6674b4007b70b95e568f528883b1af2ff51
Milo Weinberg <iapetus011@gmail.com>
2021-03-12 18:07:52 -0500

    optimize?

--- petus/main.py
+++ petus/main.py
@@ -38,16 +38,16 @@ def distance(y1: int, z1: int, x1: int, y2: int, z2: int, x2: int) -> float:
 def remove_sphere(chunks: dict, y: int, z: int, x: int, radius: int) -> None:
     for y2 in range(y - radius, y + radius):
         for z2 in range(z - radius, z + radius):
+            cz = z2 // 16
+
             for x2 in range(x - radius, x + radius):
                 d = distance(y, z, x, y2, z2, x2)
+
                 if d < radius:
-                    cx = math.floor(x2 / 16)
-                    cz = math.floor(z2 / 16)
+                    cx = x2 // 16
 
                     try:
-                        if chunks[cx, cz][y2][z2 % 16][x2 % 16] != 1 and not (
-                            int(d) == radius - 1 and chunks[cx, cz][y2][z2 % 16][x2 % 16] == 4
-                        ):  # bedrock
+                        if chunks[cx, cz][y2][z2 % 16][x2 % 16] == 4 and int(d) != radius - 1:
                             chunks[cx, cz][y2][z2 % 16][x2 % 16] = 0  # air
                     except Exception as e:
                         print(e)
